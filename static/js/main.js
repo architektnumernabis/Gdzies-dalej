238f2964b0b67c455c3c5e544d09fdac9d497921
Maciej Pawlica <maciejpawlica@MacBook-Air-Maciej.local>
2022-09-11 23:16:33 +0200

    added mobie navigation

---
+++ static/js/main.js
@@ -0,0 +1,18 @@
+const navMobile = document.querySelector('.nav-mobile__links')
+const navBtn = document.querySelector('.hamburger')
+const footerYear = document.querySelector('.footer__year')
+
+
+const handleNav = () => {
+    navBtn.classList.toggle('is-active')
+    navMobile.classList.toggle('nav-mobile__links--active')
+}
+
+navBtn.addEventListener('click', handleNav)
+
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+
+handleCurrentYear();
