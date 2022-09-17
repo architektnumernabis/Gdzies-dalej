2c26c58c1ec1d34a803ed81ded2c9c44cf3faac2
Maciej Pawlica <maciejpawlica@MacBook-Air-Maciej.local>
2022-09-18 00:15:19 +0200

    fixed js for handling mobie nav

--- static/js/main.js
+++ static/js/main.js
@@ -3,12 +3,21 @@ const navBtn = document.querySelector('.hamburger')
 const footerYear = document.querySelector('.footer__year')
 const navigationMobile = document.querySelector('.nav-mobile')
 const header = document.querySelector('.header')
+const navMobileItems = document.querySelectorAll('.nav-item')
 
 
 const handleNav = () => {
     navBtn.classList.toggle('is-active')
     navMobile.classList.toggle('nav-mobile__links--active')
     document.body.classList.toggle('body-no-scroll')
+
+
+    navMobileItems.forEach(item => {
+        item.addEventListener('click', () => {
+            navMobile.classList.remove('nav-mobile__links--active')
+            navBtn.classList.remove('is-active')
+        })
+    })
 }
 
 navBtn.addEventListener('click', handleNav)
